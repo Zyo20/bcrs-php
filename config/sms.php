9ceb60f2bfe7a1232b5418bf2546b9ff59a59a3b
Zyo20 <vocaloid5888@gmail.com>
2025-04-20 22:52:39 +0800

    remove sms api config

--- config/sms.php
+++ config/sms.php
@@ -8,11 +8,11 @@
 
 return [
     // Twilio credentials from https://www.twilio.com/console
-    'account_sid' => 'ACcc4fb6e6c7278ea1d6c9aa6805ea66c9', // Your Twilio Account SID
-    'auth_token' => '41d0a733b8fe44c033a168587cf9c518',  // Your Twilio Auth Token
+    'account_sid' => '', // Your Twilio Account SID
+    'auth_token' => '',  // Your Twilio Auth Token
     
     // Your Twilio phone number (must be purchased from Twilio)
-    'from_number' => '+15709091652',  // Format: +1XXXXXXXXXX (with country code)
+    'from_number' => '',  // Format: +1XXXXXXXXXX (with country code)
     
     // API endpoint (should not need to change this)
     'api_url' => 'https://api.twilio.com/2010-04-01/Accounts/',
